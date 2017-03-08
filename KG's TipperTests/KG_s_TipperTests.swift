ddf40f856820c84f555986a529dff2340190a263
Karpaga Ganesh Patchirajan <karpagaganesh_patchirajan@intuit.com>
2017-03-08 00:53:58 -0600

    Initial Commit

---
+++ KG's TipperTests/KG_s_TipperTests.swift
@@ -0,0 +1,36 @@
+//
+//  KG_s_TipperTests.swift
+//  KG's TipperTests
+//
+//  Created by Patchirajan, Karpaga Ganesh on 3/8/17.
+//  Copyright © 2017 Patchirajan, Karpaga Ganesh. All rights reserved.
+//
+
+import XCTest
+@testable import KG_s_Tipper
+
+class KG_s_TipperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
